23f87fa985dbde2a390a85e5ad32a889865c88e8
marigut <mari.gutierrez96@yahoo.com>
2021-05-25 19:51:40 -0500

    engineer test passed

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,25 @@
+const Engineer = require('../lib/Engineer');
+
+test('creates engineer object', () => {
+  const engineer = new Engineer('Sam Smith', 123, 'sam@gmail.com', 'sam-smith');
+
+  expect(engineer.name).toBe('Sam Smith');
+  expect(engineer.id).toBe(123);
+  expect(engineer.email).toBe('sam@gmail.com');
+  expect(engineer.github).toBe('sam-smith');
+
+});
+
+test('gets engineers github', () => {
+  const engineer = new Engineer('Sam Smith', 123, 'sam@gmail.com', 'sam-smith');
+
+  expect(engineer.getGithub()).toEqual(expect.stringContaining('sam-smith'));
+
+});
+
+test('returns Engineer', () => {
+  const engineer = new Engineer('Sam Smith', 123, 'sam@gmail.com', 'sam-smith');
+
+  expect(engineer.getRole()).toEqual(expect.stringContaining('engineer'));
+
+});
